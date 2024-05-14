8fbe291ef46ff3a40d56e02406108da872d08a75
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-05-14 15:09:32 +0800

    finished profile stats ui

--- justtweet/src/components/profilecover.js
+++ justtweet/src/components/profilecover.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Image } from 'react-bootstrap';
-import '../App.css'; 
+import '../App.css';
 
-const ProfileCover = () => {
-  return (
-    <div className="profile-cover">
-      <Image
-        src="https://pbs.twimg.com/profile_banners/1566463268/1714619053/1500x500" // Replace with your cover image URL
-        alt="Profile Cover"
-        fluid
-        className="cover-image"
-      />
-    </div>
-  );
-};
+class ProfileCover extends Component {
+  render() {
+    return (
+      <div className="profile-cover">
+        <Image
+          src="https://pbs.twimg.com/profile_banners/1566463268/1714619053/1500x500" 
+          alt="Profile Cover"
+          fluid
+          className="cover-image"
+        />
+      </div>
+    );
+  }
+}
 
 export default ProfileCover;
