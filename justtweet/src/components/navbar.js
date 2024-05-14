9de8738921ab0d265369b64eb95643c19a40a12f
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-05-14 15:47:36 +0800

    modified navbar layout

--- justtweet/src/components/navbar.js
+++ justtweet/src/components/navbar.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap';
 
 class NavbarComponent extends Component {
   render() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#">JustTweet</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#moments">Moments</Nav.Link>
-            <Nav.Link href="#notifications">Notifications</Nav.Link>
-            <Nav.Link href="#messages">Messages</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search Twitter" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
-          <Button variant="primary" className="ml-2">Tweet</Button>
-        </Navbar.Collapse>
+        <Container>
+          <Row className="w-100">
+            <Col>
+              <Navbar.Brand href="#">JustTweet</Navbar.Brand>
+            </Col>
+            <Col className="d-flex justify-content-center">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#moments">Moments</Nav.Link>
+                <Nav.Link href="#notifications">Notifications</Nav.Link>
+                <Nav.Link href="#messages">Messages</Nav.Link>
+              </Nav>
+            </Col>
+            <Col className="d-flex justify-content-end align-items-center">
+              <Form inline className="mr-3">
+                <FormControl type="text" placeholder="Search Twitter" className="mr-sm-2" />
+              </Form>
+              <Button variant="primary">Tweet</Button>
+            </Col>
+          </Row>
+        </Container>
       </Navbar>
     );
   }
