cb498ccb7305fbc1d04c610c898d3145a0570436
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-05-19 21:51:09 +0800

    update profile stats dynamically

--- justtweet/src/components/profilestats.js
+++ justtweet/src/components/profilestats.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
-class ProfileStats extends Component {
-  render() {
-    return (
-      <Container className="profile-stats">
-        <Row>
-          <Col className="stat-item">
-            <div>Tweets</div>
-            <div>51</div>
-          </Col>
-          <Col className="stat-item">
-            <div>Following</div>
-            <div>420</div>
-          </Col>
-          <Col className="stat-item">
-            <div>Followers</div>
-            <div>583</div>
-          </Col>
-          <Col className="stat-item">
-            <div>Likes</div>
-            <div>241</div>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+const ProfileStats = ({ tweetCount }) => {
+  return (
+    <Container className="profile-stats">
+      <Row>
+        <Col className="stat-item">
+          <div>Tweets</div>
+          <div>{tweetCount}</div>
+        </Col>
+        <Col className="stat-item">
+          <div>Following</div>
+          <div>420</div>
+        </Col>
+        <Col className="stat-item">
+          <div>Followers</div>
+          <div>583</div>
+        </Col>
+        <Col className="stat-item">
+          <div>Likes</div>
+          <div>241</div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default ProfileStats;
